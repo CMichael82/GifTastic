7553dbf953fb107c68cf0032c403ee947f6c9494
CAM <34009133+CMichael82@users.noreply.github.com>
2018-09-01 14:52:06 -0600

    added omdb api and updated styling

--- assets/js/gif.js
+++ assets/js/gif.js
@@ -1,38 +1,58 @@
-var topics = ["Blizzard", "Clouds", "Cyclone", "Derecho", "Fog", "Hailstorm", "Hurricane", "Lightening", "Rainbow", "Snowstorm", "Windy"];
+var topics = ["Kurt Cobain", "Joan Baez", "John Lennon", "Amy Winehouse", "Nina Simone", "Bob Dylan", "Keith Richards", "Michael Jackson", "Jamie Foxx", "Elvis Presley", "Cher"];
 
 var $addButtons = $("#addButtons");
-var $showImages = $("#showImages");
+var $showResult = $("#showResult");
 
 function displayGifs() {
-	var weather = $(this).attr("data-topic");
-	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-		weather + "&api_key=2GJ0qp4kXP8LCKLPFVPuiylHFmdAsNea";
+	var artist = $(this).attr("data-topic");
+	var giphyURL = "https://api.giphy.com/v1/gifs/search?q=" +
+		artist + "&api_key=2GJ0qp4kXP8LCKLPFVPuiylHFmdAsNea&limit=10";
+
+	var omdbURL = "https://www.omdbapi.com/?t=" + artist + "&y=&plot=short&apikey=de79b866";
+	
+	$.ajax({
+		url: omdbURL,
+		method: "GET"
+	}).then(function (movieResponse) {
+		console.log(movieResponse);
+		var movieDiv = $("<div class='movieDiv'>");
+		var title = movieResponse.Title;
+		var showTitle = $("<p>").text("Title: " + title);
+		var plot = movieResponse.Plot;
+		var showPlot = $("<p>").text("Plot: " + plot);
+		var year = movieResponse.Year;
+		var showYear = $("<p>").text("Released: " + year);
+		var posterUrl = movieResponse.Poster;
+		var showPoster = $("<img id='poster'>").attr("src", posterUrl);
+		showPoster.attr("alt", "Poster Unavailable");
+		movieDiv.append(showTitle, showPlot, showYear, showPoster);
+		$showResult.prepend(movieDiv);
+
+	});
 
 	$.ajax({
-		url: queryURL,
+		url: giphyURL,
 		method: "GET"
 	}).then(function (response) {
 		console.log(response);
 
-		var results = response.data;
-		for (var i = 0; i < 10; i++) {
-			var gifDiv = $("<div>");
-			var stillURL = results[i].images.fixed_height_still.url;
-			var animateURL = results[i].images.fixed_height.url;
+		var gifResults = response.data;
+		for (var i = 0; i < gifResults.length; i++) {
+			var gifDiv = $("<div class='gifDiv'>");
+			var rating = gifResults[i].rating;
+			var displayRating = $("<p>").text("Rating: " + rating)
+			var stillURL = gifResults[i].images.fixed_height_still.url;
+			var animateURL = gifResults[i].images.fixed_height.url;
 			var image = $("<img class='gif'>")
 			image.attr("src", stillURL);
 			image.attr("data-still", stillURL);
 			image.attr("data-state", "still");
 			image.attr("data-animate", animateURL);
-			gifDiv.append(image);
-			var rating = results[i].rating;
-			var displayRating = $("<p>").text("Rating: " + rating)
-			gifDiv.append(displayRating);
-			$showImages.prepend(gifDiv);
+			gifDiv.append(displayRating, image);
+			$showResult.prepend(gifDiv);
 		}
 	});
 }
-
 
 function renderButtons() {
 	$addButtons.empty();
@@ -47,13 +67,11 @@ function renderButtons() {
 }
 
 $("#addTopic").on("click", function (event) {
-	var weather = $("#topicInput").val().trim();
+	var artist = $("#topicInput").val().trim();
 	event.preventDefault();
-	topics.push(weather);
+	topics.push(artist);
 	renderButtons();
 });
-
-
 
 function animatePause() {
 	var state = $(this).attr("data-state");
@@ -68,5 +86,5 @@ function animatePause() {
 
 renderButtons();
 
-	$(document).on("click", ".btn-info", displayGifs);
-	$(document).on("click", ".gif", animatePause);
+$(document).on("click", ".btn-info", displayGifs);
+$(document).on("click", ".gif", animatePause);
